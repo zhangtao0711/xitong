4f2bb69f00e8d5e3805766b5751dff316ba95569
zhangtao <291293886@qq.com>
2020-05-14 14:00:47 +0800

    xitong

---
+++ src/main/java/com/cy/SuixinguanApplication.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuixinguanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuixinguanApplication.class, args);
+	}
+
+}
